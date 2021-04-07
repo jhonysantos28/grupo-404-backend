c3276feb1a5ab08fc1e30321a20003eb3fb22410
bahamonde <bahamondealine@gmail.com>
2021-04-07 10:14:41 -0300

    Implementando multer para imagem

--- src/routes/Product.js
+++ src/routes/Product.js
@@ -3,10 +3,22 @@ const router = express.Router();
 
 const productController = require('../controllers/ProductController');
 
-router.get('/', productController.getCollection)
-router.post('/', productController.insertProduct);
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/')
+    },
+    filename: function(req, file, cb) {
+        cb(null, new Date().toISOString() + file.originalname)
+    }
+});
+const upload = multer({ storage: storage });
+
+router.get('/', productController.getCollection);
+router.post('/', upload.single('img') ,productController.insertProduct);
 router.get('/:id', productController.getProduct);
 router.put('/:id', productController.updateProduct);
 router.delete('/:id', productController.deleteProduct);
+
 
 module.exports = router;
